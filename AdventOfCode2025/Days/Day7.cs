901f195db33d6cd74648cab27ba97353b5c6f9b8
Calin Catalin <71898535+catalincc4@users.noreply.github.com>
2024-12-22 18:21:55 +0200

    Day7

---
+++ AdventOfCode2025/Days/Day7.cs
@@ -0,0 +1,70 @@
+﻿namespace AdventOfCode2025.Days;
+
+public class Day7
+{
+    public static void ExecutePart1(string[] lines)
+    {
+        var equations = ParseLines(lines);
+        var sumOfCorrectEquations = CheckEquations(equations);
+        Console.WriteLine(sumOfCorrectEquations);
+    }
+
+    private static long CheckEquations(List<(long resultOfEquation, List<long> numbers)> equations)
+    {
+        long sumOfCorrectEquations = 0;
+        foreach (var equation in equations)
+        {
+            if (CheckEquation(equation, 1, equation.numbers[0]))
+            {
+                sumOfCorrectEquations += equation.resultOfEquation;
+            }
+        }
+
+        return sumOfCorrectEquations;
+    }
+
+    private static bool CheckEquation((long resultOfEquation, List<long> numbers) equation,int startIndex, long result)
+    {
+        if(equation.resultOfEquation == result && equation.numbers.Count == startIndex)
+        {
+            return true;
+        }
+
+        for (int i = startIndex; i < equation.numbers.Count; i++)
+        {
+            return CheckEquation(equation, i + 1, result + equation.numbers[i]) ||
+                   CheckEquation(equation, i + 1, result * equation.numbers[i]) ||
+                   CheckEquation(equation, i + 1, long.Parse(result.ToString() + equation.numbers[i].ToString()));
+        }
+
+        return false;
+    }
+
+    private static List<(long resultOfEquation, List<long> numbers)> ParseLines(string[] lines)
+    {
+        var equations = new List<(long resultOfEquation, List<long> numbers)>();
+        foreach (var line in lines)
+        {
+            equations.Add(ParseLine(line));
+        }
+
+        return equations;
+    }
+    private static (long resultOfEquation, List<long> numbers) ParseLine(string line)
+    {
+        var parts = line.Split(":");
+        var resultOfEquation = long.Parse(parts[0]);
+
+        List<long> numbers = new();
+        var numbersParts = parts[1].Split(" ");
+        foreach (var number in numbersParts)
+        {
+            if (long.TryParse(number, out var numberValue))
+            {
+                numbers.Add(numberValue);
+            }
+        }
+
+        return (resultOfEquation, numbers);
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2025
             var fileContent = File.ReadAllText(filePath);
             var lines = fileContent.Split("\n");
 
-            Day6.ExecutePart1(lines);
+            Day7.ExecutePart1(lines);
         }
     }
 }
